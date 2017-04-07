d25d8bc8ee42c3d4f8b58df3c08cf0c5a2c650f8
Lukas Bacevičius <lukasbacevicius@gmail.com>
2017-04-07 13:32:45 +0200

    Update lazyload.js

--- src/lazyload.js
+++ src/lazyload.js
@@ -5,67 +5,63 @@
      * @param {object} element
      */
     var lazyload = function (element) {
-        this.$element = $(element);
+        this.$doc = $(doc);
 
         this.init();
     };
 
     lazyload.prototype = {
         init: function () {
-            this.assemble();
+            this.$doc.find('noscript').each((index, noscript) => {
+                const lazyImage = $(noscript).prev()[0];
+
+                this.assemble(noscript, lazyImage);
+            });
+
 
             $(window).on('resize scroll', () => {
-                // Images were created and appended, no need for init
-                this.makeImageVisible();
+                this.$doc.find('noscript').each((index, noscript) => {
+                    const lazyImage = $(noscript).prev()[0];
+
+                    // Images were created and appended, no need for init
+                    setTimeout(() => {
+                        this.makeImageVisible(noscript, lazyImage);
+                    }, 1000 / 60);
+                });
             });
         },
-        assemble: function () {
-            //Create image
-            this.createImage();
-
-            //Append image to DOM
-            this.appendImage();
-
+        assemble: function (noscript, lazyImage) {
             //Copy attributes from noscript and add them to image
-            this.addAttributes();
+            this.addAttributes(noscript, lazyImage);
+            lazyImage.style = 'position:static; width:1px; height: 1px;';
 
-            if (this.isInViewport(this.lazyImage)) {
-                this.makeImageVisible();
+            if (this.isInViewport(lazyImage)) {
+                this.makeImageVisible(noscript, lazyImage);
             }
         },
-        makeImageVisible: function () {
-            if (this.$element[0].hasAttribute('data-lazyload-src') && this.isInViewport(this.lazyImage)) {
-                this.addSource();
+        makeImageVisible: function (noscript, lazyImage) {
+            if (noscript.hasAttribute('data-lazyload-src') && this.isInViewport(lazyImage)) {
+                this.addSource(noscript, lazyImage);
             }
         },
-        createImage: function () {
-            this.lazyImage = new Image();
-            this.lazyImage.src = 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs%3D'; // empty gif
-
-            // Element needs some size so we can detect if it's visible in user's viewport
-            this.lazyImage.style = 'width:1px; height:1px';
-        },
-        addAttributes: function () {
-            $.each(this.$element.prop('attributes'), (index, attribute) => {
-                $(this.lazyImage).attr(attribute.name, attribute.value || '');
+        addAttributes: function (noscript, lazyImage) {
+            $.each($(noscript).prop('attributes'), (index, attribute) => {
+                $(lazyImage).attr(attribute.name, attribute.value || '');
             });
         },
-        addSource: function () {
-            this.lazyImage.style = '';
+        addSource: function (noscript, lazyImage) {
+            lazyImage.style = '';
 
-            this.lazyImage.setAttribute('src', this.lazyImage.getAttribute('data-lazyload-src'));
-            this.$element[0].removeAttribute('data-lazyload-src');
+            lazyImage.setAttribute('src', lazyImage.getAttribute('data-lazyload-src'));
+            noscript.removeAttribute('data-lazyload-src');
 
             // Animate image in
-            this.lazyImage.onload = () => {
+            lazyImage.onload = () => {
                 // Throttle animation
                 setTimeout(() => {
-                    this.lazyImage.removeAttribute('data-lazyload-src');
-                }, 1000/60);
+                    lazyImage.removeAttribute('data-lazyload-src');
+                }, 1000 / 60);
             };
-        },
-        appendImage: function () {
-            this.$element[0].parentNode.insertBefore(this.lazyImage, this.$element[0]);
         },
         isInViewport: function (element) {
             let rect = element.getBoundingClientRect();
@@ -74,7 +70,7 @@
              * Only checking vertical position
              * for images positioned outside
              * of the viewport but vetically is in viewport
-            */
+             */
             return (
                 rect.top >= 0 &&
                 rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
